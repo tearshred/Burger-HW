2cf2ace3aafac69f2da82d6ca2789368265b5dcb
tearshred <vmiskeljin@gmail.com>
2019-03-20 16:48:31 -0700

    final commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('House Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gourmet Burger', false);
